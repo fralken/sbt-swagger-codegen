2e7666b720fb764ab7c7b7d0994829dbf2948e88
Dai Akatsuka <d.akatsuka@gmail.com>
2018-05-02 10:46:32 +0900

    Add warn log

--- plugin/src/main/scala/eu/unicredit/swagger/SwaggerCodegenPlugin.scala
+++ plugin/src/main/scala/eu/unicredit/swagger/SwaggerCodegenPlugin.scala
@@ -161,7 +161,8 @@ object SwaggerCodegenPlugin extends AutoPlugin {
           generateJson = swaggerGenerateJsonRW.value,
           targetDir = swaggerModelCodeTargetDir.value.getAbsoluteFile,
           modelGenerator = swaggerModelCodeGenClass.value,
-          jsonGenerator = swaggerJsonCodeGenClass.value
+          jsonGenerator = swaggerJsonCodeGenClass.value,
+          logger = sLog.value
         )
       },
       swaggerServerCodeGen := {
@@ -210,7 +211,8 @@ object SwaggerCodegenPlugin extends AutoPlugin {
                               generateJson: Boolean,
                               targetDir: File,
                               modelGenerator: ModelGenerator,
-                              jsonGenerator: JsonGenerator): Seq[File] = {
+                              jsonGenerator: JsonGenerator,
+                              logger: Logger): Seq[File] = {
 
     checkFileExistence(sourcesDir)
     IO delete targetDir
@@ -225,7 +227,9 @@ object SwaggerCodegenPlugin extends AutoPlugin {
         try {
           Some(fName -> modelGenerator.generate(fPath, codegenPackage))
         } catch {
-          case _: Exception => None
+          case _: Exception =>
+            logger.warn(s"Invalid swagger format: ${file.getCanonicalPath}")
+            None
         }
       }).flatten.toMap
 
